0e63543d383c4b9f4fa1082694f94f035bc0393b
Amartya Aishwarya <amartya970914@gmail.com>
2021-01-17 12:58:40 +0530

    fetching city details from api

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,25 @@
+const cityName = document.getElementById("cityName");
 const submitBtn = document.getElementById("submitBtn");
 
-const getInfo = (event) => {
+const city_name = document.getElementById("city_name");
+let t_key = "b14425a6554d189a2d7dc18a8e7d7263";
+
+const getInfo = async (event) => {
   event.preventDefault();
-  alert("eh yep");
+  let cityVal = cityName.value;
+  // alert("eh yep");
+  if (cityVal === "") {
+    city_name.innerText = `Please give city name before searching...`;
+  } else {
+    try {
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=df8e6f88248f09190415815adb50cfcc`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      city_name.innerText = `Please enter right city name...`;
+    }
+  }
 };
 
 submitBtn.addEventListener("click", getInfo);
